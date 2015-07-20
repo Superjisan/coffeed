3a4b3359a80646112b413ca91bcb574cf81fd652
Jisan Zaman <jzaman@wesleyan.edu>
2015-07-20 11:00:23 -0400

    created hello world app

--- coffeed/urls.py
+++ coffeed/urls.py
@@ -1,3 +1,4 @@
+
 """coffeed URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -15,7 +16,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+import core.views as coreviews
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', coreviews.TestView)
 ]
